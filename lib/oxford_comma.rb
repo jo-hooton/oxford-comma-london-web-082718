8a004fd85e69121d4505eb31168fa150e99be94c
Jo Hooton <joanneelizabethhooton@gmail.com>
2018-08-21 22:14:07 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,7 @@ def oxford_comma(array)
   elsif array.size == 2
     array.join( "and" )
   elsif array.size == 3
-    puts ([array(0)]", "[array(1)]" and "[array(2)])
+    puts (array(0)", "array(1)" and "array(2))
   else array.size > 3
     puts "something"
   end
