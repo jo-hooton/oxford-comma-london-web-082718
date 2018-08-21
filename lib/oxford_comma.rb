ec4b7a6218948a08bb4032179b36adb231c2c052
Jo Hooton <joanneelizabethhooton@gmail.com>
2018-08-21 22:29:16 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.size == 1
+      array.join
+    elsif array.size == 2
+      array[-2] << " and "
+       array.join
+    elsif array.size >= 3
+      array [-1].prepend "and "
+    array.join(', ')
+  end
 end
