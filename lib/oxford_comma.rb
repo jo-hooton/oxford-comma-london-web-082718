a0cbf82e2b1a5163449a54843c4556e1beb121c0
Jo Hooton <joanneelizabethhooton@gmail.com>
2018-08-21 22:12:04 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-  array.join
+  if array.size == 1
+    array.join
+  elsif array.size == 2
+    array.join( "and" )
+  elsif array.size == 3
+    puts ([array(0)]", "[array(1)]" and "[array(2)])
+end
+
+def 
+  array.join( "and" )
 end
